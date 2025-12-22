10875497f1c72abccb0eb74ef3605b75fa27ee0b
MJLee00 <408471330@qq.com>
2025-12-22 12:40:53 +0200

    update

--- run_on_lumi.sh
+++ run_on_lumi.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=project_462001050  # project account to bill 
 #SBATCH --partition=standard-g         # other options are small-g and standard-g
-#SBATCH --gpus-per-node=2            # Use all available GPUs on the node
+#SBATCH --gpus-per-node=1            # Use all available GPUs on the node
 #SBATCH --ntasks-per-node=1          # Single task controlling multiple GPUs
 #SBATCH --cpus-per-task=56           # Allocate full CPU socket per node for 8 GPUs
 #SBATCH --mem-per-gpu=60G            # CPU RAM per GPU (GPU memory is always 64GB per GPU)
-#SBATCH --time=8:00:00               # time limit
+#SBATCH --time=24:00:00               # time limit
 
 # this module facilitates the use of singularity containers on LUMI
 module use  /appl/local/containers/ai-modules
@@ -17,17 +17,63 @@ ENV=${RESOURCE_DIR}/ifdp-venv/bin/activate
 
 
 singularity exec "${SIF}" \
-    bash -lc  "\$WITH_CONDA  &&\
-        source ${ENV} &&\
-        python ablation.py \
-            --model-type efficientnet \
-            --k 2048 \
-            --epochs 100 \
-            --target-epsilon 4.0 \
-            --delta 1e-5 \
-            --dp-param-count 20000 \
-            --clip-radius 1.5 \
-            --run-mia \
-            --users 100 \
-            --calibration-k 200 \
-            --dp-sat-mode fisher"
+bash -lc "\
+\$WITH_CONDA && \
+source ${ENV} && \
+python ablation.py \
+  --dataset cifar10 \
+  --model-type efficientnet \
+  --k 2048 \
+  --epochs 100 \
+  --target-epsilon 1.0 \
+  --delta 1e-5 \
+  --dp-param-count 20000 \
+  --clip-radius 2.0 \
+  --run-mia \
+  --users 100 \
+  --calibration-k 200 \
+  --dp-sat-mode fisher \
+  --cuda-devices 0 && \
+
+python ablation.py \
+  --dataset cifar10 \
+  --model-type efficientnet \
+  --k 2048 \
+  --epochs 100 \
+  --target-epsilon 2.0 \
+  --delta 1e-5 \
+  --dp-param-count 20000 \
+  --clip-radius 2.0 \
+  --run-mia \
+  --users 100 \
+  --calibration-k 200 \
+  --dp-sat-mode fisher && \
+
+python ablation.py \
+  --dataset cifar10 \
+  --model-type efficientnet \
+  --k 2048 \
+  --epochs 100 \
+  --target-epsilon 3.0 \
+  --delta 1e-5 \
+  --dp-param-count 20000 \
+  --clip-radius 2.0 \
+  --run-mia \
+  --users 100 \
+  --calibration-k 400 \
+  --dp-sat-mode fisher && \
+
+python ablation.py \
+  --dataset cifar10 \
+  --model-type efficientnet \
+  --k 2048 \
+  --epochs 100 \
+  --target-epsilon 4.0 \
+  --delta 1e-5 \
+  --dp-param-count 20000 \
+  --clip-radius 2.0 \
+  --run-mia \
+  --users 100 \
+  --calibration-k 400 \
+  --dp-sat-mode fisher 
+"
